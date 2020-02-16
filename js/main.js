78826a47a5e22818466a763876e9a2ffebfc4cdd
albertbyrone <albertbyrone1677@gmail.coim>
2020-02-16 15:37:26 +0300

    add jquery to handle loader

--- js/main.js
+++ js/main.js
@@ -274,3 +274,33 @@ jQuery(window).on('load', function() {
 
 
 
+
+
+
+
+jQuery(window).on('load', function() {
+	"use strict";
+    
+    
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader");   
+    
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
+
+        $("#intro .animation-container").each(function() {
+
+            var e = $(this);
+
+            setTimeout(function(){
+
+                e.addClass("run-animation");
+
+            }, e.data("animation-delay") );
+
+        });
+
+    }, 700 );
+
+    
+});
